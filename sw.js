1728d33d6a55b08df08603173fd2ed7de884f152
Princehinkle <spiritualbuilders@gmail.com>
2025-10-28 20:53:30 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-// Content of the original Sw.js file
+  
+self.addEventListener('install', e => {  
+  e.waitUntil(caches.open('sst-v1').then(cache => cache.addAll([  
+    './','./index.html','./styles.css','./app.js','./manifest.webmanifest'  
+  ])));  
+});  
+self.addEventListener('fetch', e => {  
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));  
+});  
