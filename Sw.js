4a4cff6808c58dc6248bc18aa2459f3efa52eec1
Princehinkle <spiritualbuilders@gmail.com>
2025-10-28 21:01:14 -0500

    Delete Sw.js

--- Sw.js
+++
@@ -1,9 +0,0 @@
-  
-self.addEventListener('install', e => {  
-  e.waitUntil(caches.open('sst-v1').then(cache => cache.addAll([  
-    './','./index.html','./styles.css','./app.js','./manifest.webmanifest'  
-  ])));  
-});  
-self.addEventListener('fetch', e => {  
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));  
-});  
