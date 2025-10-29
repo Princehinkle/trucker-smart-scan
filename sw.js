fc29fd3bac7b8f453878dac034f109317e6dad8a
Princehinkle <spiritualbuilders@gmail.com>
2025-10-29 01:32:35 -0500

    Register service worker on window load

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
-  
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js').catch(console.error);
+  });
+}
 self.addEventListener('install', e => {  
   e.waitUntil(caches.open('sst-v1').then(cache => cache.addAll([  
     './','./index.html','./styles.css','./app.js','./manifest.webmanifest'  
